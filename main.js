ba44c1286972e1de4a6a5110b07712d071555e4c
jack mcqueen <jdmcqueen44@gmail.com>
2022-07-20 11:12:35 +0100

    Move form access to global scope; add form reset on close

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-const formAccess = () => {
-    const newBook = document.querySelector(".actions svg");
-    const form = document.querySelector(".form-cntr");
-    const close = document.querySelector(".form-cntr svg");
-    const page = document.querySelector("#page");
-    
-    const openForm = () => {
-        form.setAttribute("style", "display: flex;");
-        page.style.opacity = "0.3";
-    }
-    
-    const closeForm = () => {
-        form.setAttribute("style", "display: none;");
-        page.style.opacity = "1";
-    }
-    
-    newBook.addEventListener("click", openForm);
-    close.addEventListener("click", closeForm);
+const newBook = document.querySelector(".actions svg");
+const formCntr = document.querySelector(".form-cntr");
+const form = document.querySelector("form");
+const close = document.querySelector(".form-cntr svg");
+const page = document.querySelector("#page");
+
+const openForm = () => {
+    formCntr.setAttribute("style", "visibility: visible;");
+    page.style.opacity = "0.3";
 }
 
-window.addEventListener("load", formAccess);
+const closeForm = () => {
+    page.style.opacity = "1";
+    formCntr.setAttribute("style", "visibility: none;");
+    form.reset();
+}
+
+newBook.addEventListener("click", openForm);
+close.addEventListener("click", closeForm);
