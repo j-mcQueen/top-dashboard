bfed348fbddc5776a9c1da4180076cb30b3e4fca
jack mcqueen <jdmcqueen44@gmail.com>
2022-07-20 11:49:06 +0100

    Revert "Move essential dom selectors inside a function"
    This reverts commit 0152119085574f8d72c0a57c6e0c4987c103b274.

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-const essential = () => {
-    const newBook = document.querySelector(".actions svg");
-    const formCntr = document.querySelector(".form-cntr");
-    const form = document.querySelector("form");
-    const close = document.querySelector(".form-cntr svg");
-    const page = document.querySelector("#page");
-}
-essential();
+const newBook = document.querySelector(".actions svg");
+const formCntr = document.querySelector(".form-cntr");
+const form = document.querySelector("form");
+const close = document.querySelector(".form-cntr svg");
+const page = document.querySelector("#page");
 
 const openForm = () => {
     formCntr.setAttribute("style", "visibility: visible;");
     page.style.opacity = "0.3";
 }
-newBook.addEventListener("click", openForm);
 
 const closeForm = () => {
     page.style.opacity = "1";
     formCntr.setAttribute("style", "visibility: none;");
     form.reset();
 }
+
+newBook.addEventListener("click", openForm);
 close.addEventListener("click", closeForm);
