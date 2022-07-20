14e9fa867d69b73dbffea9c84f0cc47de84faac9
jack mcqueen <jdmcqueen44@gmail.com>
2022-07-20 12:04:40 +0100

    Set up Book constructor

--- main.js
+++ main.js
@@ -3,17 +3,26 @@ const formCntr = document.querySelector(".form-cntr");
 const form = document.querySelector("form");
 const close = document.querySelector(".form-cntr svg");
 const page = document.querySelector("#page");
+const bookList = document.querySelector(".favourites")
 
 const openForm = () => {
     formCntr.setAttribute("style", "visibility: visible;");
     page.style.opacity = "0.3";
 }
+newBook.addEventListener("click", openForm);
 
 const closeForm = () => {
     page.style.opacity = "1";
     formCntr.setAttribute("style", "visibility: none;");
     form.reset();
 }
-
-newBook.addEventListener("click", openForm);
 close.addEventListener("click", closeForm);
+
+const library = [];
+
+function Book(title, author, pages, read) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read;
+}
